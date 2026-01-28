13dad14990f47659ed7f5a2ea61c538ca092818c
AkashBS2005 <akashbs1023@gmail.com>
2026-01-28 20:14:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function BMICALC() {
-  let weightinput = prompt("Enter your wieght:");
-  let heightinput = prompt("Enter your height:");
+  let weightinput = prompt("Enter your wieght in kg:");
+  let heightinput = prompt("Enter your height in cm:");
 
   let weight = parseFloat(weightinput);
   let height = parseFloat(heightinput);
@@ -8,4 +8,6 @@ function BMICALC() {
   let heightcm = height / 100;
   let bmi = weight / (heightcm * heightcm);
   document.getElementById("result").innerHTML = ` Your BMI is :${bmi.toFixed(2)}`;
+  document.getElementById("para").innerHTML =
+    `Body Mass Index (BMI) is a standard measurement used to evaluate whether a person has a healthy body weight based on their height. It provides a simple numeric value that helps identify potential weight-related health problems.`;
 }
