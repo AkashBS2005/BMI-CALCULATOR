a252599462233905711cc233f120ce04592e666e
AkashBS2005 <akashbs1023@gmail.com>
2026-02-02 21:43:37 +0530

    updated

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ function BMICALC() {
   let bmi = weight / (heightcm * heightcm);
   document.getElementById("result").innerHTML = ` Your BMI is :${bmi.toFixed(2)}`;
   document.getElementById("para").innerHTML =
-    `Body Mass Index (BMI) is a standard measurement used to evaluate whether a person has a healthy body weight based on their height. It provides a simple numeric value that helps identify potential weight-related health problems.`;
+    `Body Mass Index (BMI) is a simple numerical value used to assess whether a person’s body weight is appropriate for their height. It is calculated by dividing a person’s weight in kilograms by the square of their height in meters (kg/m²). BMI helps categorize individuals into underweight, normal weight, overweight, or obese groups. It is widely used as a basic health screening tool to identify potential risks related to being underweight or overweight, such as malnutrition, heart disease, diabetes, and other lifestyle-related conditions. Although BMI is easy to calculate and useful for large populations, it does not directly measure body fat and may not be fully accurate for athletes, children, or elderly individuals. Therefore, BMI is best used along with other health assessments.`;
+
+
+if (bmi < 18.5) {
+    document.getElementById("result1").innerHTML = `You are Underweight`;
+      document.getElementById("result2").innerHTML =
+        ` Tips: Eat nutrient-rich foods, increase calorie intake, and include strength training.`;
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+   document.getElementById("result1").innerHTML =
+     ` You are Normal - Healthy`;
+} else if (bmi >= 25 && bmi <= 29.9) {
+    document.getElementById("result1").innerHTML =
+      `You are Overweight`;
+} else {
+   document.getElementById("result1").innerHTML = ` Obese`;
+}
 
   }
  
