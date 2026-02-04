b82fed5ef2465a96741effa124950b1b1c473772
AkashBS2005 <akashbs1023@gmail.com>
2026-02-04 21:29:19 +0530

    updated

--- script.js
+++ script.js
@@ -7,29 +7,39 @@ function BMICALC() {
 
   let heightcm = height / 100;
   let bmi = weight / (heightcm * heightcm);
-  document.getElementById("result").innerHTML = ` Your BMI is :${bmi.toFixed(2)}`;
   document.getElementById("para").innerHTML =
-    `Body Mass Index (BMI) is a simple numerical value used to assess whether a person’s body weight is appropriate for their height. It is calculated by dividing a person’s weight in kilograms by the square of their height in meters (kg/m²). BMI helps categorize individuals into underweight, normal weight, overweight, or obese groups. It is widely used as a basic health screening tool to identify potential risks related to being underweight or overweight, such as malnutrition, heart disease, diabetes, and other lifestyle-related conditions. Although BMI is easy to calculate and useful for large populations, it does not directly measure body fat and may not be fully accurate for athletes, children, or elderly individuals. Therefore, BMI is best used along with other health assessments.`;
+    ``;
 
 
-if (bmi < 18.5) {
+if (bmi >=1 && bmi<=18.5) {
+  document.getElementById("result").innerHTML =
+    ` Your BMI is :${bmi.toFixed(2)}`;
     document.getElementById("result1").innerHTML = `You are Underweight`;
       document.getElementById("result2").innerHTML =
         ` Tips: Eat nutrient-rich foods, increase calorie intake, and include strength training.`;
-} else if (bmi >= 18.5 && bmi <= 24.9) {
+} else if (bmi >= 18.6 && bmi <= 24.9) {
+  document.getElementById("result").innerHTML =
+    ` Your BMI is :${bmi.toFixed(2)}`;
    document.getElementById("result1").innerHTML =
      ` You are Normal - Healthy`;
        document.getElementById("result2").innerHTML =
          `Tips: Maintain a balanced diet, exercise regularly, stay hydrated, and get enough sleep. `;
 } else if (bmi >= 25 && bmi <= 29.9) {
+  document.getElementById("result").innerHTML =
+    ` Your BMI is :${bmi.toFixed(2)}`;
     document.getElementById("result1").innerHTML =
       `You are Overweight`;
       document.getElementById("result2").innerHTML =
         `Tips: Reduce junk food, exercise daily, control portion sizes, and stay active. `;
-} else {
+} else if (bmi>=30 && bmi<=99.9){
+  document.getElementById("result").innerHTML =
+    ` Your BMI is :${bmi.toFixed(2)}`;
    document.getElementById("result1").innerHTML = ` Obese`;
    document.getElementById("result2").innerHTML =
      ` Tips: Follow a healthy diet plan, do regular physical activity, avoid sugary foods, and consult a doctor.`;
+}
+else{
+  document.getElementById("result1").innerHTML='';
 }
 
   }
